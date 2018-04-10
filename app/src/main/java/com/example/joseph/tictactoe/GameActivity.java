ed9af1f9bb5299ad84e26d737064b186f9ea5080
josetedd <josetedd@gmail.com>
2018-04-10 13:29:24 +0300

    Added multiplayer logic on 3*3

--- app/src/main/java/com/example/joseph/tictactoe/GameActivity.java
+++ app/src/main/java/com/example/joseph/tictactoe/GameActivity.java
@@ -5,9 +5,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button[][] buttons =new Button[3][3];
+    private Button[][] buttons = new Button[3][3];
     private boolean player1Turn = true;
     private int roundCount;
     private int player1Points;
@@ -27,11 +28,11 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
 
         // assign buttons to buttons array
-        for(int i =0; i<3; i++){
-            for (int j=0;j<3;j++){
-                String buttonID = "button_"+i+j;
-                int resID= getResources().getIdentifier(buttonID,"id", getPackageName());
-                buttons[i][j]= findViewById(resID);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                String buttonID = "button_" + i + j;
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                buttons[i][j] = findViewById(resID);
                 buttons[i][j].setOnClickListener(this);
             }
 
@@ -40,6 +41,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         resetBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                resetBoard();
 
             }
         });
@@ -47,54 +49,106 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        if(!((Button) view).getText().toString().equals("")){
+        if (!((Button) view).getText().toString().equals("")) {
             return;
         }
-        if(player1Turn){
+        if (player1Turn) {
             ((Button) view).setText("X");
 
-        }
-        else {
+        } else {
             ((Button) view).setText("0");
 
         }
         roundCount++;
+        if (checkWin()) {
+            if (player1Turn) {
+                player1Wins();
+            } else {
+                player2Wins();
+            }
+        } else if (roundCount == 9) {
+            draw();
+        } else {
+            player1Turn = !player1Turn;
+        }
 
     }
-    private Boolean checkWin(){
-        String[][] field =new String[3][3];
-        for(int i =0; i<3; i++){
-            for (int j=0;j<3;j++){
-               field[i][j]=  buttons[i][j].getText().toString();
+
+    private Boolean checkWin() {
+        String[][] field = new String[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                field[i][j] = buttons[i][j].getText().toString();
             }
         }
         //check columns
-        for(int i =0; i<3; i++){
-            if(field[i][0].equals(field[i][1])&& field[i][0].equals(field[i][2])
-                    &&!field[i][0].equals("")){
+        for (int i = 0; i < 3; i++) {
+            if (field[i][0].equals(field[i][1]) && field[i][0].equals(field[i][2])
+                    && !field[i][0].equals("")) {
                 return true;
             }
 
         }
         //check rows
-        for(int i =0; i<3; i++){
-            if(field[0][i].equals(field[1][i])&& field[0][i].equals(field[2][i])
-                    &&!field[0][i].equals("")){
+        for (int i = 0; i < 3; i++) {
+            if (field[0][i].equals(field[1][i]) && field[0][i].equals(field[2][i])
+                    && !field[0][i].equals("")) {
                 return true;
             }
 
         }
         //check Diagonals.
         // top left to bottom right
-        if(field[0][0].equals(field[1][1])&& field[0][0].equals(field[2][2])
-                &&!field[0][0].equals("")){
+        if (field[0][0].equals(field[1][1]) && field[0][0].equals(field[2][2])
+                && !field[0][0].equals("")) {
             return true;
         }
         // top right to bottom left
-        if(field[0][2].equals(field[1][1])&& field[0][2].equals(field[2][0])
-                &&!field[0][2].equals("")){
+        if (field[0][2].equals(field[1][1]) && field[0][2].equals(field[2][0])
+                && !field[0][2].equals("")) {
             return true;
         }
         return false;
+
+    }
+
+    // method for player 1 wins
+    public void player1Wins() {
+        player1Points++;
+        Toast.makeText(this, "Player 1 wins", Toast.LENGTH_SHORT).show();
+        updatePoints();
+        resetBoard();
+    }
+
+    public void player2Wins() {
+        player2Points++;
+        Toast.makeText(this, "Player 2 wins", Toast.LENGTH_SHORT).show();
+        updatePoints();
+        resetBoard();
+
+    }
+
+    public void draw() {
+
+        Toast.makeText(this, "Draw", Toast.LENGTH_SHORT).show();
+        resetBoard();
+    }
+
+    public void updatePoints() {
+        displayP1Points.setText("Play 1 Score:" + player1Points);
+        displayP2Points.setText("Play 2 Score:" + player2Points);
+
+    }
+
+    public void resetBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+            }
+        }
+        roundCount = 0;
+        player1Points = 0;
+        player2Points = 0;
+        player1Turn = true;
     }
 }
